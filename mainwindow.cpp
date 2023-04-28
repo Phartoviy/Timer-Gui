01f115d6d5a0b07f1eb2a854f23794e78329622a
Phartoviy <uran0215@mail.ru>
2023-04-28 22:23:31 +0300

    Complete project Timer on QT

---
+++ mainwindow.cpp
@@ -0,0 +1,108 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFile>
+#include <QTimer>
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    timer = new QTimer();
+    ui->label->setNum(a);
+    ui->label_2->setNum(b);
+    connect(timer, SIGNAL(timeout()), this, SLOT(updatetime()));
+    timer->setInterval(10);
+
+
+    ui->lone->hide();
+    ui->ltwo->hide();
+    ui->lthree->hide();
+
+    setWindowFlags(Qt::CustomizeWindowHint);
+    setWindowFlag(Qt::WindowCloseButtonHint);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+    delete timer;
+}
+
+void MainWindow::updatetime()
+{
+    if (b==59)
+           {
+        b=0;
+        c++;
+        ui->label_2->setNum(b);
+        ui->label_4->setNum(c);
+    }
+    if (a>=99){
+        b++;
+        a=0;
+        ui->label->setNum(a);
+        ui->label_2->setNum(b);}
+    else{
+        a++;
+        ui->label->setNum(a);}
+
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+
+    //QTimer *timer = new QTimer(this);
+
+
+    if (key==false){
+
+        timer->start(10);
+        key=true;
+        ui->pushButton->setText("Stop");
+    }
+    else
+    {
+        key=false;
+        timer->stop();
+        ui->pushButton->setText("Start");
+    }
+
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    a = 0;
+    b = 0;
+    c = 0;
+    ui->label_4->setNum(c);
+    ui->label_2->setNum(b);
+    ui->label->setNum(a);
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    if (s==0)
+    {
+        ui->lone->hide();
+        ui->ltwo->hide();
+        ui->lthree->hide();
+    }
+    s++;
+    if (s==1)
+    {
+        ui->lone->setText(ui->label_4->text()+ui->label_5->text()+ui->label_2->text()+ui->label_3->text()+ui->label->text());
+        ui->lone->show();
+    }
+    if (s==2)
+    {
+        ui->ltwo->setText(ui->label_4->text()+ui->label_5->text()+ui->label_2->text()+ui->label_3->text()+ui->label->text());
+        ui->ltwo->show();
+    }
+    if (s==3)
+    {
+        ui->lthree->setText(ui->label_4->text()+ui->label_5->text()+ui->label_2->text()+ui->label_3->text()+ui->label->text());
+        ui->lthree->show();
+        s=0;
+    }
+}
---
+++ mainwindow.h
@@ -0,0 +1,36 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTimer>
+#include <QTime>
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    bool key = false;
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int s = 0;
+private slots:
+    void updatetime();
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QTimer *timer;
+};
+#endif // MAINWINDOW_H
